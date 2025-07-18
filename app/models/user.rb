e8460105c14e91fce8228e8b31dffb3828f7af6f
harubo <haru.act01@gmail.com>
2025-07-18 20:38:23 +0900

    make ActiveHash & Sign up view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :job_num, :first_name, :last_name, :first_name_kana, :last_name_kana,
+            presence: true
+  validates :department_id, numericality: { other_than: 1, message: "を選択してください" }
 end
