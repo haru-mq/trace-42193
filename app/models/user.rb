ce8498a252bb1a6cef53bfcd7e12d0d3dd8c49cd
harubo <haru.act01@gmail.com>
2025-07-19 10:49:29 +0900

    setting validates & create cars_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,17 @@ class User < ApplicationRecord
   
   validates :job_num, :first_name, :last_name, :first_name_kana, :last_name_kana,
             presence: true
+
+  validates :job_num,         format: { with: /\A[0-9]{8}+\z/, message: "8桁の数字を入力してください" }
+  
+  with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "を入力してください"} do
+    validates :first_name
+    validates :last_name
+  end
+  with_options  format: { with: /\A[ァ-ヶー]+\z/, message: "を入力してください"} do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
   validates :department_id, numericality: { other_than: 1, message: "を選択してください" }
 end
