abf36d9859c147f1fb03381d612fce58a61edee7
harubo <haru.act01@gmail.com>
2025-07-19 13:51:12 +0900

    TestCode for user_registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,19 @@ class User < ApplicationRecord
   validates :job_num, :first_name, :last_name, :first_name_kana, :last_name_kana,
             presence: true
 
-  validates :job_num,         format: { with: /\A[0-9]{8}+\z/, message: "8桁の数字を入力してください" }
+  validates :job_num,         format: { with: /\A[0-9]{8}\z/, message: "must be 8 digits" }, allow_blank: true
   
-  with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "を入力してください"} do
+  with_options  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "must contain only full-width Japanese characters"} do
     validates :first_name
     validates :last_name
   end
-  with_options  format: { with: /\A[ァ-ヶー]+\z/, message: "を入力してください"} do
+
+  with_options  format: { with: /\A[ァ-ヶー]+\z/, message: "must be full-width katakana"} do
     validates :first_name_kana
     validates :last_name_kana
   end
 
-  validates :department_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :password, format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]+\z/, message: "must include at least one lowercase letter, one uppercase letter, and one digit"}
+
+  validates :department_id, numericality: { other_than: 1, message: "must be other than 1" }
 end
