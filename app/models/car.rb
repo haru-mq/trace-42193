04f0403a0ea606b5226a690081a1919bffccd8a5
harubo <haru.act01@gmail.com>
2025-07-20 08:54:48 +0900

    Setting validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ApplicationRecord
+
+  belongs_to :user
+
+  validates :car_name, :launch_date, :calculation, :user, presence: true
   
 end
--- db/migrate/20250719045643_create_cars.rb
+++ db/migrate/20250719045643_create_cars.rb
@@ -2,7 +2,7 @@ class CreateCars < ActiveRecord::Migration[7.1]
   def change
     create_table :cars do |t|
       t.string      :car_name, null: false
-      t.date        :launch_date, null: false
+      t.date        :launch_date
       t.string      :calculation, null: false
       t.references  :user,  null: false, foreign_key: true
       t.timestamps
