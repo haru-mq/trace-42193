b08845e3d77e9595e3adf64d797514c2574e34c6
harubo <haru.act01@gmail.com>
2025-07-19 14:05:49 +0900

    Make Car_model

---
+++ db/migrate/20250719045643_create_cars.rb
@@ -0,0 +1,11 @@
+class CreateCars < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cars do |t|
+      t.string      :car_name, null: false
+      t.date        :launch_date, null: false
+      t.string      :calculation, null: false
+      t.references  :user,  null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
