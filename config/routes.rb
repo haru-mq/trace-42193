e71f8491f5d92958f0afe2cfae0bfe4b39fdc4ce
harubo <haru.act01@gmail.com>
2025-07-20 09:02:54 +0900

    Car index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "cars#index"
+
+  resources :cars, only: :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_18_104236) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_19_045643) do
+  create_table "cars", charset: "utf8mb3", force: :cascade do |t|
+    t.string "car_name", null: false
+    t.date "launch_date"
+    t.string "calculation", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.integer "department_id", null: false
     t.integer "job_num", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_07_18_104236) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cars", "users"
 end
