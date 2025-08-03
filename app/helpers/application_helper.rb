7be0e11052483b3a65c06479e6d0e44395cb5487
harubo <haru.act01@gmail.com>
2025-08-03 10:44:46 +0900

    Revise View

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def history_event_name(event)
+    case event
+      when "create" then "作成"
+      when "update" then "更新"
+      when "destroy" then "削除"
+    end
+  end
+
+  def history_model_name(model)
+    case model
+      when "Car" then "車両"
+      when "Calculation" then "演算"
+      when "Signalinfo" then "信号"
+    end
+  end
 end
